30edb97d8dcdbae067b80aa12c4a76a70397b2e2
Suleiman <suleosman73@gmail.com>
2023-11-09 21:58:38 +0300

    Added some comments to explain some code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import pages from pages directory
+
 import Home from "./pages/Home";
 import CountryInfo from "./pages/CountryInfo";
 import Error from "./pages/Error";
 
 // import component(s) from components directory
+
 import ScrollButton from "./components/ScrollButton";
 
 function App() {
   // Wrap the app in the router component and add an inner route for each page...
+
   return (
     <Router>
       <Routes>
