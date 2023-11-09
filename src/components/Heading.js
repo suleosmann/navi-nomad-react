2f177018dd2a1ca852b530061c517ab442de70e6
Suleiman <suleosman73@gmail.com>
2023-11-09 21:17:40 +0300

    Deleted uncessary files and code

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,8 @@
 import React from "react";
+import './heading.css'
 
 const Heading = () => {
-  return <h1> Countries App </h1>;
+  return <h1> Navi<span>Nomad</span> </h1>;
 };
 
 export default Heading;
